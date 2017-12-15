f26ee53403c1de9706a1dcf865d8f8ba0720284f
bidkar <bidkar@gmail.com>
2017-12-14 19:37:01 -0700

    all() getById() methods

--- src/models/user.php
+++ src/models/user.php
@@ -1,5 +1,7 @@
 <?php
 namespace Src\Models;
+require '../app/mysql.php';
+use Src\App\MySQL;
 
 class User {
     private $data = [
@@ -23,17 +25,59 @@ class User {
         if (array_key_exists($var, $this->data))
             $this->data[$var] = $value;
     }
+
+    public static function all() {
+        // regresar todos los usuarios de la tabla users
+        $cnn = new MySQL(); // conectamos a base de datos
+        $sql = "select * from users";
+        $rst = $cnn->query($sql); // 1: mysqli_result 2: false
+        $cnn->close();
+        if (!$rst) {
+            die('Error en la consulta: ' . $sql);
+        } elseif ($rst->num_rows > 0) {
+            $usuarios = [];
+            while ($u = $rst->fetch_assoc()) {
+                $usuario = new User();
+                $usuario->id = $u['id'];
+                $usuario->name = $u['name'];
+                $usuario->firstname = $u['firstname'];
+                $usuario->lastname = $u['lastname'];
+                $usuario->email = $u['email'];
+                array_push($usuarios, $usuario);
+            }
+        } else {
+            return false;
+        }
+
+        return $usuarios; // arreglo de objetos tipo User
+    }
+
+    public static function getById($id) {
+        $cnn = new MySQL(); // conectamos a base de datos
+        $sql = sprintf("select * from users where id = %d", $id);
+        $rst = $cnn->query($sql); // 1: mysqli_result 2: false
+        $cnn->close();
+        if (!$rst) {
+            die('Error en la consulta: ' . $sql);
+        } elseif ($rst->num_rows > 0) {
+            $u = $rst->fetch_assoc();
+            $usuario = new User();
+            $usuario->id = $u['id'];
+            $usuario->name = $u['name'];
+            $usuario->firstname = $u['firstname'];
+            $usuario->lastname = $u['lastname'];
+            $usuario->email = $u['email'];
+        } else {
+            return false;
+        }
+
+        return $usuario;
+    }
     
 }
 
-$usuario = new User();
-$usuario->name = 'bidkar';
-echo $usuario->name;
-// $usuario->name = 'bidkar'; error
-// $usuario->setId(1);
-// echo $usuario->getId();
+$usuarios = User::all();
+var_dump($usuarios);
 
-// $arreglo = [1,2,3,4,'otro'];
-// echo $arreglo[3]; // 4
-// $asociativo = ['id'=>1, 'name'=>'bidkar'];
-// echo $asociativo['name']; // bidkar
+$usuario = User::getById(1);
+var_dump($usuario);
