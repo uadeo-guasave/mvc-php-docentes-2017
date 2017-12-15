7577b06c7835c7158e7a4ef6ccaf5ed028238d8d
bidkar <bidkar@gmail.com>
2017-12-14 18:18:21 -0700

    __get __set added

--- src/models/user.php
+++ src/models/user.php
@@ -2,28 +2,38 @@
 namespace Src\Models;
 
 class User {
-    private $id = null;
-    private $name = '';
-    private $password = '';
+    private $data = [
+        'id' => null,
+        'name' => null,
+        'password' => null,
+        'firstname' => null,
+        'lastname' => null,
+        'email' => null
+    ];
 
-    public function getId() {
-        return $this->id;
+    public function __get($var) {
+        if (array_key_exists($var, $this->data))
+            return $this->data[$var];
+        else {
+            return false;
+        }
     }
-
-    public function setId($value) {
-        $this->id = $value;
+    
+    public function __set($var, $value) {
+        if (array_key_exists($var, $this->data))
+            $this->data[$var] = $value;
     }
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function getPassword() {
-        return $this->password;
-    }
+    
 }
 
 $usuario = new User();
+$usuario->name = 'bidkar';
+echo $usuario->name;
 // $usuario->name = 'bidkar'; error
-$usuario->setId(1);
-echo $usuario->getId();
+// $usuario->setId(1);
+// echo $usuario->getId();
+
+// $arreglo = [1,2,3,4,'otro'];
+// echo $arreglo[3]; // 4
+// $asociativo = ['id'=>1, 'name'=>'bidkar'];
+// echo $asociativo['name']; // bidkar
